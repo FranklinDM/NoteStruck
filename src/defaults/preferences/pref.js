097714e6c9b08cee8f074b9c45660ddf5c3080c4
FranklinDM <mrmineshafter17@gmail.com>
2017-08-21 16:29:07 +0800

    Change ext. id pref and align comments

--- src/defaults/preferences/pref.js
+++ src/defaults/preferences/pref.js
@@ -1,24 +1,24 @@
-﻿pref("extensions.amin.eft_bmnotes@gmail.com.description", "chrome://qfnotes/locale/description.properties");
+﻿pref("extensions.notestruck@franklindm.description", "chrome://qfnotes/locale/description.properties");
 
 /**Internal*/
     //Editor types plain, orion
 pref("extensions.InBasic.QuickFoxNotes.editorType", "");
     //
-pref("extensions.InBasic.QuickFoxNotes.openedIn", 0);    //1: window; 2: Tab; 4: Application content
+pref("extensions.InBasic.QuickFoxNotes.openedIn", 0);						//1: window; 2: Tab; 4: Application content
     //Repository
-pref("extensions.InBasic.QuickFoxNotes.repositoryType", 0);    //0: Use Bookmarks; 1: Use Local file
-pref("extensions.InBasic.QuickFoxNotes.RootName", "bookmarksMenuFolder");    //
-pref("extensions.InBasic.QuickFoxNotes.FolderName", "NoteStruck");        //This is the folder name which QFN will use as its primary repository [Bookmarks]
-pref("extensions.InBasic.QuickFoxNotes.lFolderName", "");                    //This is the folder name which QFN will use as its primary repository [local file/Seperate mode]
+pref("extensions.InBasic.QuickFoxNotes.repositoryType", 0);    				//0: Use Bookmarks; 1: Use Local file
+pref("extensions.InBasic.QuickFoxNotes.RootName", "bookmarksMenuFolder");   //
+pref("extensions.InBasic.QuickFoxNotes.FolderName", "NoteStruck");        	//This is the folder name which QFN will use as its primary repository [Bookmarks]
+pref("extensions.InBasic.QuickFoxNotes.lFolderName", "");                   //This is the folder name which QFN will use as its primary repository [local file/Seperate mode]
 pref("extensions.InBasic.QuickFoxNotes.sqlitePath", "");                    //This is the file name which QFN will use as its primary repository [local file/Combine mode]
-pref("extensions.InBasic.QuickFoxNotes.relativePath", false);                //Use relative path (Useful for portable edition)
+pref("extensions.InBasic.QuickFoxNotes.relativePath", false);               //Use relative path (Useful for portable edition)
     //Keyboard shortcut
 pref("extensions.InBasic.QuickFoxNotes.modifier", "accel,shift");
 pref("extensions.InBasic.QuickFoxNotes.key", "f");
-pref("extensions.InBasic.QuickFoxNotes.shortcutWindowType", 0);    //0: open in window, 1: open in tab, 2: open in app-content
+pref("extensions.InBasic.QuickFoxNotes.shortcutWindowType", 0);    			//0: open in window, 1: open in tab, 2: open in app-content
     //Backup
-pref("extensions.InBasic.QuickFoxNotes.backupPeriod", 1);    //Once per day
-pref("extensions.InBasic.QuickFoxNotes.lastBackup2", "0");    //This number is too big to be allocated as int
+pref("extensions.InBasic.QuickFoxNotes.backupPeriod", 1);    				//Once per day
+pref("extensions.InBasic.QuickFoxNotes.lastBackup2", "0");    				//This number is too big to be allocated as int
 pref("extensions.InBasic.QuickFoxNotes.numberOfBackups", 7);
     //Trash
 pref("extensions.InBasic.QuickFoxNotes.trashData", "");
@@ -28,7 +28,7 @@ pref("extensions.InBasic.QuickFoxNotes.clipboard", "");
     //Title
 pref("extensions.InBasic.QuickFoxNotes.title", "[[num]] [[type]] [path]");
     //Toolbar access method
-pref("extensions.InBasic.QuickFoxNotes.toolbarClick", 0);    //0: open in window, 1: open in tab, 2: open in app-content
+pref("extensions.InBasic.QuickFoxNotes.toolbarClick", 0);					//0: open in window, 1: open in tab, 2: open in app-content
     //Statusbar icons
 pref("extensions.InBasic.QuickFoxNotes.statusbarWindowIcon", false);
 pref("extensions.InBasic.QuickFoxNotes.statusbarTabIcon", false);
@@ -47,19 +47,19 @@ pref("extensions.InBasic.QuickFoxNotes.printStyle", "white-space: -moz-pre-wrap;
 pref("extensions.InBasic.QuickFoxNotes.addonsBlacklist", "");
     //Add-ons
 pref("extensions.InBasic.QuickFoxNotes.scriptsPath", "");
-pref("extensions.InBasic.QuickFoxNotes.scriptsInitTime", 500);        //Time before QFN tries to load extensions
-pref("extensions.InBasic.QuickFoxNotes.statusNotification", 5000);    //Show notification in statusbar for X msecs
-pref("extensions.InBasic.QuickFoxNotes.titleNotification", 2000);    //Show notification in statusbar for X msecs
-pref("extensions.InBasic.QuickFoxNotes.scriptIn", 7);    //1: only in Window; 2: only in Tab; 4: only in application content; or any combination
+pref("extensions.InBasic.QuickFoxNotes.scriptsInitTime", 500);				//Time before QFN tries to load extensions
+pref("extensions.InBasic.QuickFoxNotes.statusNotification", 5000);			//Show notification in statusbar for X msecs
+pref("extensions.InBasic.QuickFoxNotes.titleNotification", 2000);			//Show notification in statusbar for X msecs
+pref("extensions.InBasic.QuickFoxNotes.scriptIn", 7);						//1: only in Window; 2: only in Tab; 4: only in application content; or any combination
     //Initialize
 pref("extensions.InBasic.QuickFoxNotes.TimeInterval", 180000);
-pref("extensions.InBasic.QuickFoxNotes.pasteMeAfterInit", "");      //This string will be added to end of current note when QFN is initialized
-pref("extensions.InBasic.QuickFoxNotes.pasteMeAfterInitJson", "");  //JSON array to be paste after initialization
-pref("extensions.InBasic.QuickFoxNotes.argument", "");              //Import a file to QFN; This is the path of the file
-pref("extensions.InBasic.QuickFoxNotes.qfnPrefCMD", "");            //Run a command from preference
-pref("extensions.InBasic.QuickFoxNotes.firstRun", true);            //Install toolbar icon on first run
+pref("extensions.InBasic.QuickFoxNotes.pasteMeAfterInit", "");      		//This string will be added to end of current note when QFN is initialized
+pref("extensions.InBasic.QuickFoxNotes.pasteMeAfterInitJson", "");  		//JSON array to be paste after initialization
+pref("extensions.InBasic.QuickFoxNotes.argument", "");              		//Import a file to QFN; This is the path of the file
+pref("extensions.InBasic.QuickFoxNotes.qfnPrefCMD", "");            		//Run a command from preference
+pref("extensions.InBasic.QuickFoxNotes.firstRun", true);            		//Install toolbar icon on first run
     //Active tabs
-pref("extensions.InBasic.QuickFoxNotes.activeTabs", "");    //Keep active tabs name
+pref("extensions.InBasic.QuickFoxNotes.activeTabs", "");    				//Keep active tabs name
     //AgentSheet
 pref("extensions.InBasic.QuickFoxNotes.agentSheet", ".notepad ::-moz-selection {color: white; background-color: rgb(106, 124, 160);}");    //Change the color of selected text
     //Misc
@@ -68,11 +68,11 @@ pref("extensions.InBasic.QuickFoxNotes.selectedTab", 0);
 pref("extensions.InBasic.QuickFoxNotes.currentVersion", "0");
 pref("extensions.InBasic.QuickFoxNotes.openLinkInBackground", false);
 pref("extensions.InBasic.QuickFoxNotes.googleWords", "client=firefox&output=firefox&q=");
-pref("extensions.InBasic.QuickFoxNotes.copyHistory", 20);        //Maximum number of items in clipboard history
+pref("extensions.InBasic.QuickFoxNotes.copyHistory", 20);        			//Maximum number of items in clipboard history
 /**Options*/
 pref("extensions.InBasic.QuickFoxNotes.Autosave", true);
 pref("extensions.InBasic.QuickFoxNotes.SpellCheck", false);
-pref("extensions.InBasic.QuickFoxNotes.pasteOnMiddleClick", false);    //Linux problem
+pref("extensions.InBasic.QuickFoxNotes.pasteOnMiddleClick", false);    		//Linux problem
 pref("extensions.InBasic.QuickFoxNotes.copySelected", false);
 pref("extensions.InBasic.QuickFoxNotes.FontSize", "16");
 pref("extensions.InBasic.QuickFoxNotes.TabWidth", "120");
@@ -83,13 +83,13 @@ pref("extensions.InBasic.QuickFoxNotes.alwaysOnTop", false);
 pref("extensions.InBasic.QuickFoxNotes.ScrollStopOnEnd", true);
 pref("extensions.InBasic.QuickFoxNotes.ReminderSec", 0);
 pref("extensions.InBasic.QuickFoxNotes.toolbarShowtext", true);
-pref("extensions.InBasic.QuickFoxNotes.reminderWindowType", 0);    //0: open in window, 1: open in tab, 2: open in app-content
-pref("extensions.InBasic.QuickFoxNotes.timeFormat", true);    //True: 12 (AM/PM); False: 24
+pref("extensions.InBasic.QuickFoxNotes.reminderWindowType", 0);    			//0: open in window, 1: open in tab, 2: open in app-content
+pref("extensions.InBasic.QuickFoxNotes.timeFormat", true);    				//True: 12 (AM/PM); False: 24
 pref("extensions.InBasic.QuickFoxNotes.timeString", "[now] [dd]/[mm]/[yy]");
 pref("extensions.InBasic.QuickFoxNotes.showGroupInTab", true);
 pref("extensions.InBasic.QuickFoxNotes.wordWrap", true);
-pref("extensions.InBasic.QuickFoxNotes.doNoteShowInContext", true);
-pref("extensions.InBasic.QuickFoxNotes.rDelayedInits", 1);    //rDelayedInits times init time is the actual time
+pref("extensions.InBasic.QuickFoxNotes.doNoteShowInContext", true);			//True: don't show send to context menu; False: show send to context menu
+pref("extensions.InBasic.QuickFoxNotes.rDelayedInits", 1);    				//rDelayedInits times init time is the actual time
     //network
 pref("extensions.InBasic.QuickFoxNotes.evernote_username", "");
 pref("extensions.InBasic.QuickFoxNotes.sugarsync_username", "");
